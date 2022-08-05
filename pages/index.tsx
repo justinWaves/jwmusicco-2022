211bc2b620bdeb71434570631f50bf2a8ebc2300
justinWaves <juhstinn@gmail.com>
2022-08-05 13:52:20 -0700

    added video header to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,32 +15,32 @@ const Home: NextPage = () => {
         </Head>
 
         <main className="flex w-full flex-1 flex-col  items-center text-center ">
-          <div className=" hidden md:block top-10 left-10 z-10 absolute">
+          <div className="  absolute top-10 left-10 z-4 w-60 ">
             <Image
               src={jwlogo}
               alt="jwmc logo"
-              className=" cursor-pointer object-contain"
+              className=" cursor-pointer object-contain z-10"
             ></Image>
           </div>
-          <PianoMenuMobile />
-          <div className="absolute">
-            <video
-              autoPlay
-              loop
-              muted
-              className=" hidden md:block w-auto max-w-screen top-0 "
-            >
+
+          <div className="flex grid-cols-1">
+            <video autoPlay loop muted className=" w-auto max-w-screen z-0 ">
               <source src="/Recordplayer.mp4" type="video/mp4" />
             </video>
-
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path
-                fill="#ffffff"
-                fill-opacity="1"
-                d="M0,96L48,106.7C96,117,192,139,288,160C384,181,480,203,576,181.3C672,160,768,96,864,85.3C960,75,1056,117,1152,122.7C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-              ></path>
-            </svg>
           </div>
+          <PianoMenuMobile />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+            className=" bottom-0 left-0 z-10 w-screen 
+              "
+          >
+            <path
+              fill="#F1F5F9"
+              fill-opacity="1"
+              d="M0,96L48,106.7C96,117,192,139,288,160C384,181,480,203,576,181.3C672,160,768,96,864,85.3C960,75,1056,117,1152,122.7C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
         </main>
       </div>
 
